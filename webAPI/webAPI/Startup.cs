c2af480d549222564b32c732e49440b3a6a05e25
harsha1439 <Z1856779@students.niu.edu>
2020-06-24 19:01:23 -0500

    WEB API Completed

---
+++ webAPI/webAPI/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using WebAPI.Models;
+
+
+namespace webAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+               .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+               .AddJsonOptions(options => {
+                   options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                   options.JsonSerializerOptions.PropertyNamingPolicy = null;
+               });
+            services.AddDbContext<PaymentDetailContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
